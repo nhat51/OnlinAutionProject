01c1ec76e0cc80fc4a07fe2e30bd5e650b0d6e5c
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-10-26 18:55:28 +0700

    fix my account and add my wallet

---
+++ Template/js/Customize.js
@@ -0,0 +1,7 @@
+function Input(){
+    document.getElementById('firstname').removeAttribute('readonly');
+    document.getElementById('lastname').removeAttribute('readonly');
+    document.getElementById('email').removeAttribute('readonly');
+    document.getElementById('telephone').removeAttribute('readonly');
+    document.getElementById('changeButton').removeAttribute('hidden')
+}
