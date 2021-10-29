67e443b410a34c8236f8390e0adaba2d4af8fdf8
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-10-30 00:12:09 +0700

    update

--- OnlineAutionProject/OnlineAuctionMVC/Startup.cs
+++ OnlineAutionProject/OnlineAuctionMVC/Startup.cs
@@ -55,7 +55,8 @@ namespace OnlineAuctionMVC
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                
+            }); 
         }
     }
 }
